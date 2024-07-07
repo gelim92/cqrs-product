33443e28d8ec53ebd8bd95775c9685c2c49164ce
Gerard Lim <gelim@Gerards-MacBook-Pro.local>
2024-07-07 18:41:05 +0800

    setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderModule } from './order/order.module';
+import { Order } from './order/entities/order.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'mypassword',
+      username: 'myusername',
+      entities: [Order],
+      database: 'cqrs_order',
+      synchronize: true,
+      logging: true,
+    }),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/order/entities/order.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('orders')
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 250 })
+  name: string;
+}
